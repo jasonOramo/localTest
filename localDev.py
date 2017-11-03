192b3276ef7c912ccaf7e5fad6f20996cee66631
jasonOramo <jason_oramo@sina.com>
2017-11-03 21:57:20 +0800

    leetcode test

--- localDev.py
+++ localDev.py
@@ -19,4 +19,21 @@ def findStr(testStr):
 		for matchNum in lineMatches:
 			resultStr += str(key) + ',' + str(matchNum) + " "
 	print resultStr
+def reverse( x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        maxValue = (1<<31) -1
+        res = 0;
+        if x > 0:
+            strV =  str(x)[::-1]
+            res = int(strV)
+        else:
+            strV = str(x*-1)[::-1]
+            res =  int(strV) * -1
+        if res > maxValue or res < maxValue * -1:
+            return 0
+        else:
+            return res;
 findStr('abc')
