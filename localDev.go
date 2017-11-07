434121fd548d31f924bb69fe52a0782fac62479b
jasonOramo <jason_oramo@sina.com>
2017-11-07 20:43:18 +0800

    leetcode test

--- localDev.go
+++ localDev.go
@@ -23,7 +23,73 @@ func reverse(x int) int {
         return res;
     }
 }
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ 
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+        var current ListNode;
+    var nextL1, nextL2 *ListNode;
+    var tempVal,nextVal,addVal,smallVal,largerLength,smallerLength int;
+    var l1Vals,l2Vals,larger,smaller []int;
+    var eleArr []ListNode;
 
+
+	for nextL1 = l1; nextL1 != nil; nextL1 = nextL1.Next{
+		l1Vals = append(l1Vals,nextL1.Val);
+	}
+	for nextL2 = l2; nextL2 != nil; nextL2 = nextL2.Next{
+		l2Vals = append(l2Vals,nextL2.Val);
+	}
+
+    l1Length := len(l1Vals);
+    l2Length := len(l2Vals);
+
+    if(l1Length >= l2Length){
+    	largerLength = l1Length;
+    	smallerLength = l2Length;
+    	larger = l1Vals;
+    	smaller = l2Vals;
+    }else{
+    	largerLength = l2Length;
+    	smallerLength = l1Length;
+    	larger = l2Vals;
+    	smaller = l1Vals;
+    }
+
+    for j:=0; j < largerLength; j++{
+    	if(j < smallerLength){
+    		smallVal = smaller[j];
+    	}else{
+    		smallVal = 0;
+    	}
+    	tempVal = larger[j] + smallVal + addVal;
+
+		if(tempVal > 9){
+			nextVal = tempVal % 10;
+			addVal = 1;
+		}else{
+			nextVal = tempVal;
+			addVal = 0;
+		}
+		current.Val = nextVal;
+		eleArr = append(eleArr,current);
+    }
+    if(addVal != 0){
+    	current.Val = 1;
+    	eleArr[len(eleArr) -1].Next = &current;
+    	eleArr = append(eleArr,current);
+    }
+    for i :=0; i < len(eleArr) -1; i = i+1{
+    	eleArr[i].Next = &eleArr[i+1];
+    }
+
+    return &eleArr[0];
+}
 func main(){
   
 }
