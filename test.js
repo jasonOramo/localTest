4e538b425900f2290703a7bd1a20ea341d8e67ba
jasonOramo <3431636500@qq.com>
2019-07-02 21:26:55 +0800

    JS test file
    string2Int test

---
+++ test.js
@@ -0,0 +1,15 @@
+'use strict'
+
+
+function testString2Int(s){
+  if(s.length) > 1 {
+    var arr;
+    for(var i = 0; i < s.length; i++){
+      arr.push(i);
+    }
+    return arr.map(function(x){return s[i]*1;}).reduce(function(x,y){return x*10+y;});
+  }else if(s.length == 1){
+    return s[0]*1;
+  }
+  return null;
+}
