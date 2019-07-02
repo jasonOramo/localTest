722b8743aa0d832e16a4e5d67a0f28e93edc3ce8
jasonOramo <3431636500@qq.com>
2019-07-02 21:40:26 +0800

    string2Int
    Use Array.prototype.map function to deal with string.

--- test.js
+++ test.js
@@ -2,14 +2,15 @@
 
 
 function testString2Int(s){
-  if(s.length) > 1 {
-    var arr;
-    for(var i = 0; i < s.length; i++){
-      arr.push(i);
-    }
-    return arr.map(function(x){return s[i]*1;}).reduce(function(x,y){return x*10+y;});
-  }else if(s.length == 1){
-    return s[0]*1;
-  }
-  return null;
+//   if(s.length) > 1 {
+//     var arr;
+//     for(var i = 0; i < s.length; i++){
+//       arr.push(i);
+//     }
+//     return arr.map(function(x){return s[i]*1;}).reduce(function(x,y){return x*10+y;});
+//   }else if(s.length == 1){
+//     return s[0]*1;
+//   }
+//   return null;
+  return Array.prototype.map.call(s,function(x){return x*1;}).reduce(function(x,y){return x*10+y;});
 }
